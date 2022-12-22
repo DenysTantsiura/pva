7c724e17a8cab54bcd102b4bb48b33ffba27490d
Belzer104 <shablovskyiillia@gmail.com>
2022-12-22 22:18:50 +0200

    Update note_book.py

--- note_book.py
+++ note_book.py
@@ -7,8 +7,8 @@ class NoteBook(UserDict):
         """Add new record in the notebook."""
         self.data[record.name] = record
 
-
     def iterrator(self, count: int) -> list:
+        """Return records of all notebook"""
         tags = []
         
         for record in self.data.values():
@@ -25,17 +25,20 @@ class NoteBook(UserDict):
         if tags:
             yield tags
 
-
-
     def remove_record(self, name: str) -> None:
         """Remove record from notebook."""
         self.data.pop(name)
 
     def sort_by_tags(self) -> list:
+        """Return list of note-names sorted by tags."""
+
         tags = []
-        
-        for tag in self.data.value():
-            tags.
+
+        for note in self.data.values():
+            tags.extend(note.tags)
+
+        tags = list(set(tags))
+        tags.sort()
 
 
 
