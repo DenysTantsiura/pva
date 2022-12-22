9a0a3fd66ac71b5ac3328188bae008f1699018fa
Denys Tantsiura <ghost0002501@gmail.com>
2022-12-22 15:48:56 +0200

    Added to start.py main function.

--- start.py
+++ start.py
@@ -1,1 +1,17 @@
 # pva main function for start
+from typing import NoReturn
+
+
+class PVA():
+    ...
+
+    def start(self):
+        ...
+
+def main() -> NoReturn:
+    pva_start = PVA()
+    pva_start.start()
+
+
+if __name__ == '__main__':
+    main()
