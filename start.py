e075fce5cfcffa9e8a05a221ecda2d9dd6a651e2
Denys Tantsiura <ghost0002501@gmail.com>
2022-12-22 16:08:53 +0200

    Added to PVA class method 'start'.

--- start.py
+++ start.py
@@ -29,6 +29,28 @@ class PVA():
         self.contact_dictionary, self.path_file = LoadBook(self.path_file).load_book()
         self.note_book, self.path_file_notes = LoadBook(self.path_file_notes).load_book(NoteBook)
 
+    def start(self) -> NoReturn:
+        """The main function of launching a helper console bot that recognize 
+        the commands entered from the keyboard and respond according 
+        to the command entered. Enter a command - get an answer.
+        """
+        while True:
+
+            user_request = InputToParser().listen()
+
+            if user_request[0] in ALL_COMMAND_ADDRESSBOOK:  # dict of commands
+                bot_answer_result = OutputAnswer().show_out(user_request, self.contact_dictionary, self.path_file)
+            elif user_request[0] in ALL_COMMAND_NOTEBOOK:
+                bot_answer_result = OutputAnswer().show_out(user_request, self.note_book, self.path_file_notes)
+            elif user_request[0] in ALL_COMMAND_FILESORTER:
+                bot_answer_result = OutputAnswer().show_out(user_request, None, '')
+            else:
+                user_request = ['command_guesser'] + user_request
+                bot_answer_result = OutputAnswer().show_out(user_request, None, '')
+
+            if not bot_answer_result:
+                break
+
 
 def main() -> NoReturn:
     pva_start = PVA()
