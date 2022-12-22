0489d86389ee3db3f8b60d92116e83dea9c6a7fe
Denys Tantsiura <ghost0002501@gmail.com>
2022-12-22 13:28:42 +0200

    added to class Record methods: add_birthday, add_phone, add_email.

--- classes_address_book.py
+++ classes_address_book.py
@@ -4,21 +4,25 @@ from typing import Union
 class Field:
     ...
 
+
 class Address:
     ...
+
 
 class Name(Field):
     ...
 
+
 class Birthday:
     ...
+
 
 class Phone:
     ...
 
+
 class Email:
     ...
-
 
 
 class Record:
@@ -44,3 +48,40 @@ class Record:
         else:
             return False, f'Address is missing!\"{address}\"'
 
+    def add_birthday(self, birthday: str) -> tuple:
+        """Adds a new entry for the user's birthday to the address book."""
+        if not self.birthday:
+            self.birthday = Birthday(birthday)
+
+            return True,
+
+        else:
+            return False, f'Birthday already recorded for \"{self.name.value}\"You can change it.'
+
+    def add_phone(self, phone_new: str) -> bool:
+        """Adds a new entry for the user's phone to the address book."""
+        phone_new1 = Phone(phone_new)
+
+        for phone in self.phones:
+            if phone_new1 == phone.value:
+                print(f'\"{phone_new1}\" already recorded for \"{self.name.value}\"')
+
+                return False
+
+        self.phones.append(phone_new1)
+
+        return True
+    
+    def add_email(self, email_new: str) -> bool:
+        """Adds a new entry for the user's email to the address book."""
+        email_new1 = Email(email_new)
+
+        for email in self.emails:
+            if email_new1 == email.value:
+                print(f'\"{email_new1}\" already recorded for \"{self.name.value}\"')
+
+                return False
+
+        self.emails.append(email_new1)
+
+        return True
