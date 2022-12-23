ade07c806a0cf49cccd384e318ceef03c4c677ee
Denys Tantsiura <ghost0002501@gmail.com>
2022-12-23 09:31:22 +0200

    Added list commands to dict.ALL_COMMAND.

--- handlers.py
+++ handlers.py
@@ -1,1 +1,62 @@
 # hendlers...
+
+
+from typing import Union
+
+
+ALL_COMMAND_ADDRESSBOOK = {
+    '?': handler_help,
+    'add_address': handler_add_address,
+    'add_birthday': handler_add_birthday,
+    'add_email': handler_add_email,
+    'add_phone': handler_add_phone,
+    'add': handler_add,
+    'change_address': handler_change_address,
+    'change_birthday': handler_change_birthday,
+    'change_email': handler_change_email,
+    'change': handler_change,
+    'close': handler_exit,
+    'email': handler_email,
+    'exit': handler_exit,
+    'find': handler_find,
+    'good_bye': handler_exit,
+    'happy_birthday': handler_happy_birthday,
+    'hello': handler_hello,
+    'help': handler_help,
+    'phone': handler_phone,
+    'remmove_address': handler_remove_address,
+    'remove_birthday': handler_remove_birthday,
+    'remove_email': handler_remove_email,
+    'remove_phone': handler_remove_phone,
+    'remove': handler_remove,
+    'show_all': handler_show_all,
+    'show': handler_show,
+    }
+
+ALL_COMMAND_NOTEBOOK = {
+    'add_note': handler_add_note,
+    'show_notes': handler_show_notes,
+    'show_note': handler_show_note,
+    'remove_note': handler_remove_note,
+    'change_note': handler_change_note,
+    'find_notes': handler_find_notes,
+    'sort_notes': handler_sort_notes,
+}
+
+ALL_COMMAND_FILESORTER = {
+    'sort': handler_sort,
+}
+
+ALL_COMMAND = {'command_guesser': handler_command_guesser, }
+
+ALL_COMMAND.update(ALL_COMMAND_ADDRESSBOOK)
+ALL_COMMAND.update(ALL_COMMAND_NOTEBOOK)
+ALL_COMMAND.update(ALL_COMMAND_FILESORTER)
+
+
+def main_handler(user_command: list, contact_dictionary: Union[AddressBook, NoteBook], path_file: str) -> Union[str, list]:
+    """Description..."""
+    ...
+
+
+
