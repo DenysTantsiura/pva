a24a73b2f051e9af636b9615cef72d0b2c180ddd
Belzer104 <shablovskyiillia@gmail.com>
2022-12-24 01:33:04 +0200

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -5,38 +5,44 @@ from typing import Union
 from .address_book import AddressBook
 
 # @input_error
-def handler_add_birthday(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
+def handler_add_birthday(user_command: list, contact_dictionary: AddressBook, path_file: str) -> str:
     """Bot add birthday to contact"""
 
     name = user_command[1]
-    return contact_dictionary[name].add_birthday(user_command[2])
+    birthday = user_command[2]
+    contact_dictionary[name].add_birthday(birthday)
+
+    return f'Birthday {birthday} for contact {name} added' 
     
     #path_file in future
 
 
 # @input_error
-def handler_change_birthday(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
+def handler_change_birthday(user_command: list, contact_dictionary: AddressBook, path_file: str) -> str:
     """Bot has changed birthday for contact on new birthday"""
 
     name = user_command[1]
     birthday = user_command[2]
-    return contact_dictionary[name].change_birthday(birthday)
+    contact_dictionary[name].change_birthday(birthday)
+
+    return f'Birthday {birthday} for contact {name} has changed'
 
 
 # @input_error
-def handler_happy_birthday(user_command: List[str], contact_dictionary: AddressBook, _=None) -> list:
+def handler_happy_birthday(user_command: list, contact_dictionary: AddressBook, _=None) -> list:
 
     return contact_dictionary.show_happy_birthday(user_command[1])
 
 
 # @input_error
-def handler_remove_birthday(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
+def handler_remove_birthday(user_command: list, contact_dictionary: AddressBook, path_file: str) -> str:
     """Bot has removed birthday for contact. Birthday changed on 'None'"""
 
     name = user_command[1]
     
-    if name in contact_dictionary[name]:
-        return contact_dictionary[name].remove_birthday()
+    if name in contact_dictionary:
+        contact_dictionary[name].remove_birthday()
+        return f'Birthday for contact {name} delited'
 
     else:
         return f'Contact don`t found'
