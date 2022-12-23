60654fb66b3869e82a7daa53743f16e329a73606
Yaroslava Kalat <syaya@ukr.net>
2022-12-23 13:27:10 +0200

    Update class_note
    Rewrote uppercase constant and def add_tags (updated loop for: changed return ('This tag already exists!') on print and rewrote self.tags.append without join.
    Changed the tuple to str in def change_note and removed the part about changing some words.

--- class_note.py
+++ class_note.py
@@ -9,6 +9,7 @@ class Note:
         self.name = name
         self.text = text
         self.tags = []
+        
 
     def __str__(self) -> str:
         return f'{self.name}: {self.text}'
@@ -20,35 +21,32 @@ class Note:
             new_tags (str): Tags that the user wants to add.
         Returns:
             string(str): If false - the answer for the user, otherwise - a list of tags.'''
-
+        NUMBER_TAGS = 5
         add_tags = new_tags.split(', ')
-        number_tags = 5
-        if len(self.tags) > number_tags:
+        
+        if len(self.tags) > NUMBER_TAGS:
             return ('You cannot add more than five tags!')
         else:
-            delta = number_tags - len(self.tags)
+            delta = NUMBER_TAGS - len(self.tags)
             if len(add_tags) <= delta:
+                
                 for tag in add_tags:
                     if tag in self.tags:
-                        return ('This tag already exists!')
+                        print (f'This tag "{tag}" already exists!')
                     else:   
-                        self.tags.append(', '.join(add_tags))
-                        return self.tags
+                       self.tags.append(tag)
+                return self.tags
             else:
                 return (f'You can add only {delta} tags!')
    
-    def change_note(self, changed_text, new_text) -> tuple:
-        '''change_note...": The bot rewrites note or changes some words.
+    def change_note(self, new_text: str) -> str:
+        '''change_note...": The bot rewrites note.
         Parameters:
-            changed_text (str): The text the user wants to change.
             new_text (str): The new text the user wants to add.
         Returns:
             self.text: Changed text.'''
 
-        if self.text == changed_text:
-            self.text = new_text
-        else:
-            self.text = self.text.replace(changed_text, new_text)
+        self.text = new_text
         print(f'{self.name}: {self.text}')
         return self.text
        
