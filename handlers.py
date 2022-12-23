c92c15d4c2bd40f3df6ce883491c2b32572fbd03
Belzer104 <shablovskyiillia@gmail.com>
2022-12-24 00:26:52 +0200

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,44 +1,4 @@
 # @input_error
-
-def handler_add_email(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
-    """Bot add email to contact"""
-
-    name = user_command[1]
-    return contact_dictionary[name].add_email(user_command[2])
-
-# @input_error
-
-def handler_change_email(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
-    """Bot change email for contact"""
-
-    name = user_command[1]
-    return contact_dictionary[name].change_email(user_command[2], user_command[3])
-
-# @input_error
-
-def handler_email(user_command: List[str], contact_dictionary: AddressBook, _=None) -> str:
-    """Bot showed email for contact"""
-
-    name = user_command[1]
-    return f"{name} have email are: {[mail.value for mail in contact_dictionary.email]}"
-
-
-# @input_error
-
-def handler_remove_email(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
-    """Bot removed email for contact"""
-
-    name = user_command[1]
-    email = user_command[2]
-    
-    if name in contact_dictionary[name]:
-        return contact_dictionary[name].remove_email(email)
-
-    else:
-        return f'Contact don`t found'
-    
-# @input_error
-
 def handler_add_birthday(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
     """Bot add birthday to contact"""
 
@@ -49,7 +9,6 @@ def handler_add_birthday(user_command: List[str], contact_dictionary: AddressBoo
 
 
 # @input_error
-
 def handler_change_birthday(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
     """Bot has changed birthday for contact on new birthday"""
 
@@ -58,7 +17,6 @@ def handler_change_birthday(user_command: List[str], contact_dictionary: Address
     return contact_dictionary[name].change_birthday(birthday)
 
 # @input_error
-
 def handler_happy_birthday(user_command: List[str], contact_dictionary: AddressBook, _=None) -> list:
 
     return contact_dictionary.show_happy_birthday(user_command[1])
@@ -66,7 +24,6 @@ def handler_happy_birthday(user_command: List[str], contact_dictionary: AddressB
 
 
 # @input_error
-
 def handler_remove_birthday(user_command: List[str], contact_dictionary: AddressBook, path_file: str) -> str:
     """Bot has removed birthday for contact. Birthday changed on 'None'"""
 
