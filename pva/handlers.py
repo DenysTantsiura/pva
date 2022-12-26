8596dbce88560cc5639dcf5f9cec2e6719601d9d
Belzer104 <shablovskyiillia@gmail.com>
2022-12-26 21:53:27 +0200

    Update handlers.py

--- pva/handlers.py
+++ pva/handlers.py
@@ -65,7 +65,6 @@ def handler_add_note(user_command: list, note_book: NoteBook, path_file: str) ->
         record = Note(name, text)
         note_book.add_record(record)
         print (f'You added new note - {name}: {text}.')
-    if text:
 
     if tags:
         if name not in note_book:
